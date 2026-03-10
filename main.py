085dc47f1fad0481629c8251a2ed295add58da63
Ariel Zuniga <ariel.cerda10@inacapmail.cl>
2026-03-10 18:12:11 -0300

    se agrega conexion con postgreSQL y modelos

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+import models
+from database import engine
 
+
+models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI(
     title= "API  de resúmenes con IA",
---
+++ models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column,Integer,String,Text,ForeignKey,DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer,primary_key=True,index=True)
+    email = Column(String,unique=True,index=True,nullable=False)
+    password_hash = Column(String,nullable=False)
+    
+    # con la siguiente linea se establece la relacion
+    #la cual indica que un usuario puede tener muchos documentos
+    
+    documents = relationship("Document",back_populates="owner")
+    
+    
+ 
+class Document(Base):
+    __tablename__ = "documents"
+    
+    id = Column(Integer,primary_key=True,index=True)
+    original_text = Column(Text,nullable=False)
+    ai_summary = Column(Text,nullable=False)
+    created_at = Column(DateTime(timezone=True),server_default= func.now())
+    
+    #se establece la llave foranea que conecta con el usuario
+    
+    owner_id = Column(Integer,ForeignKey("users.id"))
+    
+    owner = relationship("User",back_populates="documents")
