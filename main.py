1432508dcf0de37eb6012da73b67ac2b2809c1ab
Ariel Zuniga <ariel.cerda10@inacapmail.cl>
2026-03-29 20:02:21 -0300

    Se implementa OAuth2PasswordBearer y se protege la ruta del perfil con JWT

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from database import engine, get_db
 from datetime import datetime,timedelta,timezone
 import jwt
 import os
-
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI(
@@ -28,6 +28,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES =30 #TIEMPO EN EL QUE EL TOKEN EXPIRA EN MINUTOS
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login/")
+
 def verify_password(plain_password,hashed_password):
     return pwd_context.verify(plain_password,hashed_password)
 
@@ -37,6 +39,27 @@ def create_access_token(data:dict):
     to_encode.update({"exp":expire})
     encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
     return encoded_jwt
+
+def get_current_user(token:str = Depends(oauth2_scheme),db: Session = Depends(get_db)):
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        email:str = payload.get("sub")
+        
+        if email is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "Token inválido")
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="El token ha exprirado")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Token invalido")
+    
+    user = db.query(models.User).filter(models.User.email == email).first()
+      
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "Usuario no encontrado")
+    
+    return user
+    
+        
 
 @app.get("/")
 
@@ -70,8 +93,8 @@ def crear_usuario(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 @app.post("/login/",response_model=schemas.Token)
 
-def login(user_credentials:schemas.UserCreate, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     
     if not user or not verify_password(user_credentials.password, user.password_hash):
         raise HTTPException(
@@ -82,4 +105,8 @@ def login(user_credentials:schemas.UserCreate, db: Session = Depends(get_db)):
     access_token =  create_access_token(data={"sub": user.email})
     
     return {"access_token":access_token, "token_type": "bearer"}
-        
+
+
+@app.get ("/perfil/", response_model=schemas.UserResponse)
+def obtener_perfil(usuario_actual: models.User = Depends(get_current_user)):
+    return usuario_actual
