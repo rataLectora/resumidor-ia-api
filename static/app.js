15eacd7704aa88cd5ba71c6894e4b5b45bfb450f
Ariel Zuniga <ariel.cerda10@inacapmail.cl>
2026-05-05 19:04:16 -0400

    se implementa la logica completa del chat interactivo y se corrige ciclo de envio de mensajes

--- static/app.js
+++ static/app.js
@@ -7,6 +7,9 @@ const fileInput = document.getElementById("pdf-file")
 const summaryBox = document.getElementById("summary-box")
 const chatInput= document.getElementById("chat-input")
 const btnSend = document.getElementById("btn-send")
+const chatBox =  document.getElementById("chat-box")
+
+let documentoActualId = null 
 
 
 // Se usa una funcion asincrona (async) considerando que la peticion a la API tomara unos milisegundos
@@ -79,6 +82,8 @@ btnUpload.addEventListener("click", async function () {
         if(respuesta.ok){
             const datos = await respuesta.json()
 
+            documentoActualId = datos.id
+
             summaryBox.innerHTML = `<div class="text-gray-800 leading-relaxed">${datos.ai_summary}</div>`;
 
             chatInput.disabled = false
@@ -98,5 +103,88 @@ btnUpload.addEventListener("click", async function () {
         btnUpload.disabled = false
     }
     
+})
+
+async function enviarMensaje() {
+    const pregunta  = chatInput.value.trim()
+
+    if (!pregunta || !documentoActualId) return
+
+    if (chatBox.innerHTML.includes("El chat está vacío")){
+        chatBox.innerHTML = ""
+    }
+
+    chatBox.innerHTML += `
+        <div class="flex justify-end animate-fade-in-up">
+            <div class="bg-blue-600 text-white px-5 py-3 rounded-2xl rounded-tr-sm max-w-[80%] shadow-sm">
+                ${pregunta}
+            </div>
+        </div>
+    `
+
+    chatInput.value = ""
+    chatInput.disabled = true
+    btnSend.disabled = true
+
+    chatBox.scrollTop = chatBox.scrollHeight
+
+    const  idMensajeIA = "msg-" + Date.now()
+    chatBox.innerHTML += `
+        <div class="flex justify-start">
+            <div id="${idMensajeIA}" class="bg-white border border-gray-200 text-gray-800 px-5 py-3 rounded-2xl rounded-tl-sm max-w-[80%] shadow-sm flex items-center gap-3">
+                <i class="fa-solid fa-robot text-blue-500"></i>
+                <span class="text-gray-400 italic font-medium flex items-center gap-1">
+                    Analizando el documento <i class="fa-solid fa-ellipsis fa-fade text-lg"></i>
+                </span>
+            </div>
+        </div>
+    `
+    chatBox.scrollTop = chatBox.scrollHeight
+
+    try{
+        const token  = localStorage.getItem("token")
+
+        const  respuesta = await fetch (`/documentos/${documentoActualId}/chat`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                pregunta: pregunta
+            })
+        })
+
+        const cajaIA = document.getElementById(idMensajeIA)
+
+        if(respuesta.ok){
+            const datos = await respuesta.json()
+
+            cajaIA.innerHTML = `
+                <i class="fa-solid fa-robot text-blue-500 mb-auto mt-1 text-lg"></i> 
+                <div class="leading-relaxed">${datos.respuesta}</div>
+            `
+        }else{
+            cajaIA.innerHTML = `<i class="fa-solid fa-triangle-exclamation text-red-500"></i> <span class="text-red-500 font-bold">Error al obtener respuesta del documento.</span>`;
+        }
+
+    } catch (error){
+        console.error("Error en el chat:",error)
+        document.getElementById(idMensajeIA).innerHTML = `<i class="fa-solid fa-wifi text-red-500"></i> <span class="text-red-500 font-bold">Error de conexión con el servidor.</span>`;
+    }finally{
+
+        chatInput.disabled = false
+        btnSend.disabled = false
+        chatInput.focus()
+        chatBox.scrollTop = chatBox.scrollHeight
+    }
+    
+}
+
+btnSend.addEventListener("click",enviarMensaje)
+chatInput.addEventListener("keypress", function(evento){
+    if (evento.key === "Enter"){
+        enviarMensaje()
+    }
 })
 
